435a0d34317f6c96c553ef473355df2837837205
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-11 23:54:22 -0700

    add additional tests for merchant index request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -10,6 +10,7 @@ require 'simplecov'
 SimpleCov.start do
   add_filter 'spec/'
 end
+require 'helpers'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -12,20 +12,96 @@ RSpec.describe "Merchants API" do
       merchants = JSON.parse(response.body, symbolize_names: true)
 
       expect(merchants.count).to eq(20)
+      expect(merchants.pluck(:id)).to match_array(Merchant.first(20).pluck(:id))
 
       merchants.each do |merchant|
         expect(merchant).to be_a(Hash)
         check_structure(merchant, :id, Integer)
         check_structure(merchant, :name, String)
-        expect(merchant.keys).to match_array(%i[id name])
+        expect(merchant.keys).to include(:id, :name)
       end
     end
 
-    it 'sends an array of data even if one resource is found'
-    it 'sends an array of data even if zero resources are found'
-    it 'allows for optional per_page query param'
-    it 'allows for optional page query param'
-    it 'page query param defaults to 1 if not specified by the user'
-    it 'allows the user to pass both per_page and page query params'
+    it 'sends an array of data even if one resource is found' do
+      create(:merchant)
+
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants).to be_an(Array)
+      expect(merchants.count).to eq(1)
+    end
+
+    it 'sends an array of data even if zero resources are found' do
+      get '/api/v1/merchants'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants).to be_an(Array)
+      expect(merchants.count).to eq(0)
+    end
+
+    describe 'allows for optional per_page query param' do
+      it 'users can request less than the total number of merchants' do
+        create_list(:merchant, 3)
+
+        get '/api/v1/merchants?per_page=2'
+
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(2)
+        expect(merchants.pluck(:id)).to match_array(Merchant.first(2).pluck(:id))
+      end
+
+      it 'users can request more than the total number of merchants' do
+        create_list(:merchant, 2)
+
+        get '/api/v1/merchants?per_page=3'
+
+        expect(response).to be_successful
+
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants.count).to eq(2)
+      end
+    end
+
+    it 'allows for optional page query param' do
+      create_list(:merchant, 21)
+
+      get '/api/v1/merchants?page=1'
+
+      expect(response).to be_successful
+
+      page1 = JSON.parse(response.body, symbolize_names: true)
+
+      get '/api/v1/merchants?page=2'
+
+      page2 = JSON.parse(response.body, symbolize_names: true)
+
+      expect(page1.size).to eq(20)
+      expect(page2.size).to eq(1)
+      expect(page1.pluck(:id)).not_to include(page2.pluck(:id))
+    end
+
+    it 'allows the user to pass both per_page and page query params' do
+      create_list(:merchant, 5)
+
+      get '/api/v1/merchants?per_page=3&page=2'
+
+      expect(response).to be_successful
+
+      merchants = JSON.parse(response.body, symbolize_names: true)
+
+      expect(merchants.count).to eq(2)
+      expect(merchants.pluck(:id)).to eq(Merchant.last(2).pluck(:id))
+    end
   end
 end
