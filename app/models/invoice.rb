fa2e38ecb6610f8fb920e8c31738087c0a60eb97
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-12 08:30:39 -0700

    add .rubocop.yml and fix enum syntax

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,5 +2,5 @@ class Invoice < ApplicationRecord
   belongs_to :customer
   belongs_to :merchant
 
-  enum status: [:packaged, :shipped, :returned]
+  enum status: { packaged: 0, shipped: 1, returned: 2 }
 end
--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,5 +1,5 @@
 class Transaction < ApplicationRecord
   belongs_to :invoice
 
-  enum result: [:success, :failed, :refunded]
+  enum result: { success: 0, failed: 1, refunded: 2 }
 end
