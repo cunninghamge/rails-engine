0060f640da4438db3c5074f99594b5eadb3aaf37
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-12 08:29:58 -0700

    add select_merchants method with tests

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'class methods' do
+    describe '.select_merchants' do
+      it 'gets the first 20 merchants by default' do
+        create_list(:merchant, 21)
+
+        selected = Merchant.select_merchants(nil, nil)
+        expect(selected).to eq(Merchant.first(20))
+      end
+
+      it 'gets merchants when passed a page number' do
+        create_list(:merchant, 21)
+
+        selected = Merchant.select_merchants(nil, 2)
+        expect(selected).to eq([Merchant.last])
+      end
+
+      it 'gets merchants using a per_page limit' do
+        create_list(:merchant, 3)
+
+        selected = Merchant.select_merchants(2, nil)
+        expect(selected).to eq(Merchant.first(2))
+      end
+
+      it 'gets merchants using both a page number and a per_page limit' do
+        create_list(:merchant, 3)
+
+        selected = Merchant.select_merchants(2, 2)
+        expect(selected).to eq([Merchant.last])
+      end
+    end
+  end
 end
