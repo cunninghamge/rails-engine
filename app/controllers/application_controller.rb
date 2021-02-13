5981572af7854f6d6bfdf45570f1c94bfecbdaf2
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-12 20:43:59 -0700

    add basic error serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::StatementInvalid, with: :render_invalid_parameters
+  rescue_from ActiveRecord::RecordNotFound, with: :render_resource_not_found
+
+  def render_invalid_parameters
+    render json: ErrorSerializer.invalid_parameters, status: 400
+  end
+
+  def render_resource_not_found(error)
+    render json: ErrorSerializer.record_not_found(error.message), status: 404
+  end
 end
