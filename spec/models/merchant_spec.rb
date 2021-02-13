f2c130e5d1740b3b87d37e4a99b3cfa6ed2b1f9a
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-12 20:54:52 -0700

    extract select_merchants and select_items to application_controller as select_records

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,32 +2,32 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   describe 'class methods' do
-    describe '.select_merchants' do
+    describe '.select_records' do
       it 'gets the first 20 merchants by default' do
         create_list(:merchant, 21)
 
-        selected = Merchant.select_merchants(nil, nil)
+        selected = Merchant.select_records(nil, nil)
         expect(selected).to eq(Merchant.first(20))
       end
 
       it 'gets merchants when passed a page number' do
         create_list(:merchant, 21)
 
-        selected = Merchant.select_merchants(nil, 2)
+        selected = Merchant.select_records(nil, 2)
         expect(selected).to eq([Merchant.last])
       end
 
       it 'gets merchants using a per_page limit' do
         create_list(:merchant, 3)
 
-        selected = Merchant.select_merchants(2, nil)
+        selected = Merchant.select_records(2, nil)
         expect(selected).to eq(Merchant.first(2))
       end
 
       it 'gets merchants using both a page number and a per_page limit' do
         create_list(:merchant, 3)
 
-        selected = Merchant.select_merchants(2, 2)
+        selected = Merchant.select_records(2, 2)
         expect(selected).to eq([Merchant.last])
       end
     end
