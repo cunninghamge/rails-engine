6a99506c047058ad386338ec8f8208d8ed553629
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-12 23:08:18 -0700

    add tests for deleting an item

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -3,5 +3,14 @@ FactoryBot.define do
     customer
     merchant
     status { [:packaged, :shipped, :returned].sample }
+
+    trait :with_items do
+      transient { items { create_list(:item, 2) } }
+      after(:create) do |invoice, evaluator|
+        evaluator.items.each do |item|
+          create(:invoice_item, invoice: invoice, item: item)
+        end
+      end
+    end
   end
 end
