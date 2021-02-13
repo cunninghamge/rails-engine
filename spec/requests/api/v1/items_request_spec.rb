b618313e0f1c8b330445184c844d450b6c8ed1b2
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-13 12:07:39 -0700

    add tests for updating an item

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -246,11 +246,8 @@ RSpec.describe "Items API" do
       check_hash_structure(item[:data][:attributes], :description, String)
       check_hash_structure(item[:data][:attributes], :unit_price, Float)
       check_hash_structure(item[:data][:attributes], :merchant_id, Integer)
-      check_hash_structure(item[:data][:attributes], :created_at, String)
-      check_hash_structure(item[:data][:attributes], :updated_at, String)
       expect(item[:data].keys).to match_array(%i[id type attributes])
-      attributes = %i[name description unit_price merchant_id created_at updated_at]
-      expect(item[:data][:attributes].keys).to match_array(attributes)
+      expect(item[:data][:attributes].keys).to match_array(%i[name description unit_price merchant_id])
     end
 
     it 'returns an error if any attributes are missing' do
@@ -284,8 +281,122 @@ RSpec.describe "Items API" do
       expect(created_item).not_to have_attribute(:model_number)
 
       item = JSON.parse(response.body, symbolize_names: true)
-      attributes = %i[name description unit_price merchant_id created_at updated_at]
-      expect(item[:data][:attributes].keys).to match_array(attributes)
+      expect(item[:data][:attributes].keys).to match_array(%i[name description unit_price merchant_id])
+    end
+  end
+
+  describe 'updates' do
+    it 'updates an exiting item' do
+      id = create(:item).id
+      previous_name = Item.last.name
+      item_params = { name: "Hawaiian Shirt"}
+      headers = {'CONTENT_TYPE' => 'application/json'}
+
+      patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate(item: item_params)
+
+      expect(response).to be_successful
+
+      item = Item.find(id)
+      expect(item.name).to eq(item_params[:name])
+      expect(item.name).not_to eq(previous_name)
+
+
+      body = JSON.parse(response.body, symbolize_names: true)
+
+      expect(body).to be_a(Hash)
+      check_hash_structure(body, :data, Hash)
+      check_hash_structure(body[:data], :id, String)
+      check_hash_structure(body[:data], :type, String)
+      check_hash_structure(body[:data], :attributes, Hash)
+      check_hash_structure(body[:data][:attributes], :name, String)
+      expect(body[:data][:attributes][:name]).to eq(item_params[:name])
+      check_hash_structure(body[:data][:attributes], :description, String)
+      check_hash_structure(body[:data][:attributes], :unit_price, Float)
+      check_hash_structure(body[:data][:attributes], :merchant_id, Integer)
+      expect(body[:data].keys).to match_array(%i[id type attributes])
+      expect(body[:data][:attributes].keys).to match_array(%i[name description unit_price merchant_id])
+    end
+
+    it 'can update multiple attributes' do
+      item = create(:item)
+      previous_name = item.name
+      previous_description = item.description
+      previous_unit_price = item.unit_price
+      previous_merchant_id = item.merchant_id
+      new_merchant = create(:merchant)
+      item_params = attributes_for(:item).merge(merchant_id: new_merchant.id)
+      headers = {'CONTENT_TYPE' => 'application/json'}
+
+      patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item: item_params)
+
+      expect(response).to be_successful
+
+      item = Item.find(item.id)
+      expect(item.name).to eq(item_params[:name])
+      expect(item.name).not_to eq(previous_name)
+      expect(item.description).to eq(item_params[:description])
+      expect(item.description).not_to eq(previous_description)
+      expect(item.unit_price).to eq(item_params[:unit_price])
+      expect(item.unit_price).not_to eq(previous_unit_price)
+      expect(item.merchant_id).to eq(item_params[:merchant_id])
+      expect(item.merchant_id).not_to eq(previous_merchant_id)
+    end
+
+    it 'returns an error for a bad item id' do
+      item_params = attributes_for(:item)
+      headers = {'CONTENT_TYPE' => 'application/json'}
+
+      patch "/api/v1/items/1", headers: headers, params: JSON.generate(item: item_params)
+
+      expect(response.status).to eq(404)
+    end
+
+    it 'returns an error for a bad merchant id' do
+      item = create(:item)
+      item_params = { merchant_id: item.merchant_id + 1}
+      headers = {'CONTENT_TYPE' => 'application/json'}
+
+      patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item: item_params)
+
+      expect(response.status).to eq(404)
+    end
+
+    it 'does not change the item if all attributes are missing' do
+      item = create(:item)
+      previous_name = item.name
+      previous_description = item.description
+      previous_unit_price = item.unit_price
+      previous_merchant_id = item.merchant_id
+      headers = {'CONTENT_TYPE' => 'application/json'}
+
+      patch "/api/v1/items/#{item.id}", headers: headers, params: JSON.generate(item: {})
+
+      expect(response).to be_successful
+
+      item = Item.find(item.id)
+      expect(item.name).to eq(previous_name)
+      expect(item.description).to eq(previous_description)
+      expect(item.unit_price).to eq(previous_unit_price)
+      expect(item.merchant_id).to eq(previous_merchant_id)
+    end
+
+    it 'ignores any additional attributes sent by the user' do
+      id = create(:item).id
+      previous_name = Item.last.name
+      item_params = { name: 'Hawaiian Shirt', model_number: '28P1817074' }
+      headers = {'CONTENT_TYPE' => 'application/json'}
+
+      patch "/api/v1/items/#{id}", headers: headers, params: JSON.generate(item: item_params)
+
+      expect(response).to be_successful
+
+      item = Item.find(id)
+      expect(item.name).to eq(item_params[:name])
+      expect(item.name).not_to eq(previous_name)
+      expect(created_item).not_to have_attribute(:model_number)
+
+      item = JSON.parse(response.body, symbolize_names: true)
+      expect(item[:data][:attributes].keys).to match_array(%i[name description unit_price merchant_id])
     end
   end
 
