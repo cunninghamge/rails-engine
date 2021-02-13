b4e567cab38b339f16e7c77a64f9f15cdbbe3457
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-12 18:25:17 -0700

    add Item.select_items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :merchant
+
+  def self.select_items(per_page, page)
+    results = (per_page || 20).to_i
+    skipped_pages = (page || 1).to_i - 1
+    limit(results).offset(results * skipped_pages)
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,6 @@ class Merchant < ApplicationRecord
   def self.select_merchants(per_page, page)
     results = (per_page || 20).to_i
     skipped_pages = (page || 1).to_i - 1
-    Merchant.limit(results).offset(results * skipped_pages)
+    limit(results).offset(results * skipped_pages)
   end
 end
