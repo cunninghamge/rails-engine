6eea30ce110ca7bb6c320c6181c57c42941f6344
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-14 13:03:59 -0700

    add tests for finding unshipped orders

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -7,4 +7,33 @@ RSpec.describe Invoice, type: :model do
     it { should have_many(:invoice_items).dependent(:destroy) }
     it { should have_many(:transactions).dependent(:destroy) }
   end
+
+  describe 'class methods' do
+    describe '.unshipped_orders' do
+      before(:each) do
+        11.times do |n|
+          invoice = create(:invoice, status: 'packaged')
+          create(:invoice_item, invoice: invoice, unit_price: 1.0, quantity: n + 1)
+          create(:transaction, invoice: invoice, result: 'success')
+        end
+      end
+
+      it 'selects a specified quantity of invoices ranked by potential revenue' do
+        orders = Invoice.unshipped_orders(5)
+
+        expect(orders.to_a.size).to eq(5)
+        expect(orders[0].revenue).to eq(11.0)
+        expect(orders[1].revenue).to eq(10.0)
+        expect(orders[2].revenue).to eq(9.0)
+        expect(orders[3].revenue).to eq(8.0)
+        expect(orders[4].revenue).to eq(7.0)
+      end
+
+      it 'returns 10 invoices if no quantity is specified' do
+        orders = Invoice.unshipped_orders(nil)
+
+        expect(orders.to_a.size).to eq(10)
+      end
+    end
+  end
 end
