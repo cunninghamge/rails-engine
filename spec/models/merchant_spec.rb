fdea5d6bae6aedd5ce4d1bfb4d18d849a362fdde
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-14 22:30:32 -0700

    add tests for finding top merchants by revenue

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -52,5 +52,38 @@ RSpec.describe Merchant, type: :model do
         expect(Merchant.find_one('ring')).to eq(annies_rings)
       end
     end
+
+    describe '.top_merchants' do
+      before(:each) do
+        6.times do |n|
+          merchant = create(:merchant)
+          (n + 1).times do
+            item = create(:item, merchant: merchant)
+            invoice = create(:invoice, merchant: merchant, status: 'shipped')
+            create(:invoice_item, invoice: invoice, item: item, unit_price: 1.0, quantity: 1)
+            create(:transaction, invoice: invoice, result: 'success')
+          end
+        end
+      end
+
+      it 'finds a specified quantity of merchants' do
+        merchants = Merchant.top_merchants(5)
+
+        expect(merchants.to_a.size).to eq(5)
+        expect(merchants[0].revenue).to eq(6)
+        expect(merchants[1].revenue).to eq(5)
+        expect(merchants[2].revenue).to eq(4)
+        expect(merchants[3].revenue).to eq(3)
+        expect(merchants[4].revenue).to eq(2)
+      end
+
+      it 'can find a different quantity of merchants' do
+        merchants = Merchant.top_merchants(2)
+
+        expect(merchants.to_a.size).to eq(2)
+        expect(merchants[0].revenue).to eq(6)
+        expect(merchants[1].revenue).to eq(5)
+      end
+    end
   end
 end
