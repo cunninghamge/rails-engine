ce8f0b741c287bb5b3a16af5fe4b3172ef8698da
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-14 22:37:30 -0700

    add top_merchants method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,4 +4,13 @@ class Merchant < ApplicationRecord
   def self.find_one(query)
     where('LOWER(name) LIKE ?', "%#{query.downcase}%").order(:name).first
   end
+
+  def self.top_merchants(quantity)
+    select('merchants.*, SUM(quantity * invoice_items.unit_price) revenue')
+      .joins(items: [invoice_items: [invoice: :transactions]])
+      .where(invoices: { status: 'shipped' }, transactions: { result: 'success' })
+      .group(:id)
+      .order('revenue' => :desc)
+      .limit(quantity)
+  end
 end
