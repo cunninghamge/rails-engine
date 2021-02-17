e5dd14fa066e9e83cf1160026c7e333194b76c8e
Gus Cunningham <67352677+cunninghamge@users.noreply.github.com>
2021-02-16 20:20:05 -0700

    add tests for getting merchants by items sold

--- spec/requests/api/v1/revenue/merchants_by_sales_spec.rb
+++ spec/requests/api/v1/revenue/merchants_by_sales_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'find merchants by revenue' do
+RSpec.describe 'find merchants by item quantity sold' do
   before(:each) do
-    6.times do |n|
+    8.times do |n|
       merchant = create(:merchant)
       (n + 1).times do
         item = create(:item, merchant: merchant)
@@ -13,8 +13,8 @@ RSpec.describe 'find merchants by revenue' do
     end
   end
 
-  it 'returns a quantity of merchants ranked by descending revenue' do
-    get "/api/v1/revenue/merchants?quantity=5"
+  it 'returns a quantity of merchants ranked by total items sold' do
+    get "/api/v1/merchants/most_items?quantity=7"
 
     expect(response).to be_successful
 
@@ -22,41 +22,55 @@ RSpec.describe 'find merchants by revenue' do
 
     expect(merchants).to be_a(Hash)
     check_hash_structure(merchants, :data, Array)
-    expect(merchants[:data].count).to eq(5)
+    expect(merchants[:data].count).to eq(7)
 
     merchants[:data].each do |merchant|
       expect(merchant).to be_a(Hash)
       check_hash_structure(merchant, :id, String)
       check_hash_structure(merchant, :type, String)
-      expect(merchant[:type]).to eq('merchant_name_revenue')
+      expect(merchant[:type]).to eq('items_sold')
       check_hash_structure(merchant, :attributes, Hash)
       check_hash_structure(merchant[:attributes], :name, String)
-      check_hash_structure(merchant[:attributes], :revenue, Float)
+      check_hash_structure(merchant[:attributes], :count, Integer)
       expect(merchant.keys).to match_array(%i[id type attributes])
-      expect(merchant[:attributes].keys).to match_array(%i[name revenue])
+      expect(merchant[:attributes].keys).to match_array(%i[name count])
     end
   end
 
-  it 'returns an error if a quantity is not specified' do
-    get "/api/v1/revenue/merchants"
+  it 'defaults to 5 if the quantity is not specified' do
+    get "/api/v1/merchants/most_items"
 
-    expect(response.status).to eq(400)
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(5)
+  end
+
+  it 'can return less than 5 merchants' do
+    get "/api/v1/merchants/most_items?quantity=2"
+
+    expect(response).to be_successful
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(2)
   end
 
   it 'returns an error if the quantity is not an integer' do
-    get "/api/v1/revenue/merchants?quantity=string"
+    get "/api/v1/merchants/most_items?quantity=string"
 
     expect(response.status).to eq(400)
   end
 
   it 'returns an error if the quantity is less than 0' do
-    get "/api/v1/revenue/merchants?quantity=-5"
+    get "/api/v1/merchants/most_items?quantity=-5"
 
     expect(response.status).to eq(400)
   end
 
   it 'returns an error if the quantity is blank' do
-    get "/api/v1/revenue/merchants?quantity="
+    get "/api/v1/merchants/most_items?quantity="
 
     expect(response.status).to eq(400)
   end
